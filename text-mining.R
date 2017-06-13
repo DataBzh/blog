d7f47c96562774b618af3184a0be905254835377
ColinFay <contact@colinfay.me>
2017-06-13 22:17:36 +0200

    create & changes

--- text-mining.R
+++ text-mining.R
@@ -17,7 +17,7 @@ books_tidy <- proust_books() %>%
   mutate(text = stringr::str_replace_all(.$text, "’", " ")) %>% 
   unnest_tokens(word, text) %>%
   filter(!word %in% stopwords_iso$fr) %>%
-  count(word, sort = TRUE) %a>%
+  count(word, sort = TRUE) %>%
   head(10)
 
 barplot(height=books_tidy$n, names.arg= books_tidy$word, xlab="Mots", ylab="Fréquence", col="#973232", main="À la recherche du temps perdu")
@@ -50,33 +50,53 @@ barplot(height=head(TDM,10)$freq, names.arg=head(TDM,10)$word, xlab="Mots", ylab
 
 #racinisation
 
-corpus2 <- tm_map(corpus, stemDocument)
-TDM2 <- TermDocumentMatrix(corpus2)
-TDM2 <- as.matrix(TDM2)
+library("SnowballC")
+books_tidy <- proust_books() %>%
+  mutate(text = stringr::str_replace_all(.$text, "’", " ")) %>% 
+  unnest_tokens(word, text) %>%
+  count(word, sort = TRUE)
+
+nrow(books_tidy)
+
+stem <- wordStem(books_tidy$word, language = "french")
+length(unique(stem))
 
 #Partie 3
 
 ##Création du nuage de mot
 library(wordcloud)
-wordcloud(frequence$word, frequence$freq, max.words=100, rot.per=FALSE, colors= c("#973232", "#1E5B5B", "#6D8D2F", "#287928"))
+books_tidy <- proust_books() %>%
+  mutate(text = stringr::str_replace_all(.$text, "’", " ")) %>% 
+  unnest_tokens(word, text) %>%
+  filter(!word %in% stopwords_iso$fr) %>%
+  count(word, sort = TRUE)
+wordcloud(books_tidy$word, books_tidy$n, max.words=100, rot.per=FALSE, colors= c("#973232", "#1E5B5B", "#6D8D2F", "#287928"))
 
 #Partie 4 
 
 ##Vecteur de termes associés 
+corpus <- proust_books() %>%
+  mutate(text = stringr::str_replace_all(.$text, "’", " ")) %>% 
+  .$text %>% 
+  VectorSource()%>%
+  VCorpus()%>%
+  tm_map(content_transformer(tolower))%>%
+  tm_map(stripWhitespace) %>%
+  tm_map(removeNumbers)%>%
+  tm_map(removePunctuation)%>%
+  tm_map(removeWords, stopwords("french"))%>%
+  TermDocumentMatrix() 
+
 library(qdapTools)
-associations <- findAssocs(TDM, c("king","queen"), corlimit = 0.05)
-associations_df <-data.frame(list_vect2df(associations))
-names(associations_df) <- c("source", "terme", "corr")
-library(dplyr)
-associations_df <- arrange(associations_df, desc(corr))
-library(ggplot2)
-ggplot(associations_df[1:50,], aes(x = terme, y = corr, fill=source)) + 
-  geom_bar(size = 4, stat="identity", position = "dodge") + 
+
+associations <- findAssocs(corpus, "temps", corlimit = 0.05) %>% 
+  list_vect2df() %>%
+  data.frame()
+names(associations) <- c("source", "terme", "corr")
+associations <- arrange(associations, desc(corr))
+ggplot(associations[1:10,], aes(x = terme, y = corr)) + 
+  geom_bar(stat="identity") + 
   coord_flip()+
   xlab("Terme") + 
   ylab("Corrélation") + 
-  theme(axis.text=element_text(size=10),
-        axis.title=element_text(size=15),
-        axis.title.x=element_text(margin=margin(20,0,0,0)),
-        axis.title.y=element_text(margin=margin(0,20,0,0)),
-        plot.margin=margin(20,20,20,20))
+  theme_minimal()
